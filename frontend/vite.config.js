404a94a3dff12510d4219453dc1add6a56bd44ed
Joseph Liyon PT <120583161+jliyon23@users.noreply.github.com>
2025-04-17 13:29:27 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,12 +6,4 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  server: {
-    proxy: {
-      '/socket.io': {
-        target: 'http://localhost:3001',
-        ws: true,
-      },
-    },
-  },
 })
